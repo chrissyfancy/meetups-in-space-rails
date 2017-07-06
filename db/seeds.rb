f574780a450107304208ec6476826b23d819c431
Chrissy Fancy <chrissyfancy@gmail.com>
2017-07-06 16:18:26 -0400

    Add additional information to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 # For creating fake events, that require a name, description, location, and date:
 15.times do
   Event.create!(name: "#{Faker::HowIMetYourMother.character}'s #{Faker::HowIMetYourMother.catch_phrase} Event", description: Faker::Hipster.paragraph(3..6), location: Faker::Space.galaxy, date: Faker::Date.forward(100))
@@ -16,4 +8,10 @@ topics = ["Arts & Culture", "Book Clubs", "Career & Business", "C""ars & Motorcy
 
 topics.each do |topic|
   Topic.create!(name: topic)
+end
+
+# Seed Meetups table that joins Events and Topics
+
+Event.all.each do |event|
+  Meetup.create!(event_id: event.id, topic_id: Topic.find(rand(1..Topic.all.length)).id)
 end
