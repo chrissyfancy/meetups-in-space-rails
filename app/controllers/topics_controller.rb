1e2a2376ab14145205790352044413d18972fd41
Chrissy Fancy <chrissyfancy@gmail.com>
2017-07-09 12:31:56 -0400

    Refactor topic show page

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,10 +7,5 @@ class TopicsController < ApplicationController
   def show
     @topic = Topic.find(params[:id])
     @meetups = Meetup.where(topic_id: @topic)
-    @all_meetups = []
-    @meetups.each do |meetup|
-      event = Event.find(meetup.id)
-      @all_meetups << { id: event.id, name: event.name }
-    end
   end
 end
