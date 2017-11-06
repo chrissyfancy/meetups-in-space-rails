52e21fc4d0397d66f738f4527f55cd47378ef522
Chrissy Fancy <chrissyfancy@gmail.com>
2017-11-06 14:09:50 -0500

    Update migrations

--- db/migrate/20170710194015_create_meetups.rb
+++
@@ -1,9 +0,0 @@
-class CreateMeetups < ActiveRecord::Migration[5.0]
-  def change
-    create_table :meetups do |t|
-      t.belongs_to :event
-      t.belongs_to :topic
-      t.timestamps
-    end
-  end
-end
